b83af3658fe46c3a9cbc9e36d5a19b0caf80403b
Dmitry <dzmitry.life@gmail.com>
2021-01-27 20:00:29 +0300

    add new branch for task_1

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hello world!")
+# def my_func(a, b):
+#     summ = a + b
+#     print(f"{a} + {b} = {summ}")
+#     return summ
+# my_func(4, 5)
+# my_func(10, 199)
+#
+# a = my_func(4,5)
+# print(a)
+#
