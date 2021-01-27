36d88ae2e4c9bd2bf26e16e04075a205dff85df5
Dmitry <dzmitry.life@gmail.com>
2021-01-27 21:06:58 +0300

    task_02 complete

--- task_02.py
+++ task_02.py
@@ -1,8 +1,14 @@
-def factorial(a):
-    i = 1
-    sum = 1
-    while i <= a:
-        sum *= i
-        i += 1
-    return sum
-print(factorial(5))
+# def factorial(a):
+#     i = 1
+#     sum = 1
+#     while i <= a:
+#         sum *= i
+#         i += 1
+#     return sum
+# print(factorial(5))
+
+# def my_format(string, char="!"):
+#     result_string = print(f"{char} {string} {char}")
+#     return result_string
+# my_format("HI")
+
